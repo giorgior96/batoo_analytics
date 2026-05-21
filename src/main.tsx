a23ee8b94e2d65f9cf94ce0626b045fd953e6bd4
giorgio <giorgio.giorgior@gmail.com>
2026-05-21 13:50:04 +0200

    Redirect analytics app to dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const DASHBOARD_URL = 'https://analytics.batoo.it/en/dashboard'
+
+if (window.location.href !== DASHBOARD_URL) {
+  window.location.replace(DASHBOARD_URL)
+} else {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
